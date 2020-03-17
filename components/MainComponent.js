1311d01bb4e5a4834d72973f83209de53cd29fc0
chavda-bhavik <bhavik.chvda@gmail.com>
2020-03-17 17:52:16 +0530

    Added(FavoritesComponent): Added Favorites Component that shows favorite dishes

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -12,6 +12,7 @@ import Contact from './ContactUsComponent';
 import Dishdetail from './DishDetailComponent';
 import AddComment from './AddCommentComponent';
 import Reservation from './ReservationComponent';
+import Favorites from './FavoritesComponent';
 
 import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from "../redux/ActionCreators";
 import { connect } from 'react-redux';
@@ -52,6 +53,23 @@ const ContactNavigator = createStackNavigator({
 
 const ReservationNavigator = createStackNavigator({
   Reservation: { screen: Reservation }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"            
+    },
+    headerTintColor: "#fff",
+    headerLeft: <Icon name="menu" size={24} color='#4630EB' 
+                  onPress={() => navigation.toggleDrawer()}
+                />
+  })
+});
+
+const FavoritesNavigator = createStackNavigator({
+  Favorites: { screen: Favorites }
 }, {
   navigationOptions: ({ navigation }) => ({
     headerStyle: {
@@ -178,7 +196,22 @@ const MainNavigator = createAppContainer(createDrawerNavigator({
           <Icon name='cutlery' type='font-awesome' size={24} color={tintColor} />
         )
       }, 
-    }    
+    },
+  Favorites: { 
+    screen: FavoritesNavigator,
+    navigationOptions: {
+      title: 'My Favorites',
+      drawerLabel: 'My Favorites',
+      drawerIcon: ({ tintColor }) => (
+        <Icon 
+          name='heart' 
+          type='font-awesome' 
+          size={24} 
+          color={tintColor} 
+        />
+      )
+    }, 
+  }      
 }, {
   drawerBackgroundColor: '#D1C4E9',
   contentComponent: CustomDrawerContentComponent
