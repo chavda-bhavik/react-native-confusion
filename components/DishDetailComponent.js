0476ad60b1aabf07719fa382b531e2831a0d74c2
chavda-bhavik <bhavik.chvda@gmail.com>
2020-03-21 11:57:19 +0530

    Added(RubberBandGesture): Added RubberBand Gesture to Dishdetail

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -21,6 +21,8 @@ const mapDispatchToProps = dispatch => ({
 
 const RenderDish = (props) => {
     const dish = props.dish
+    let view
+    let handleViewRef = ref => { view = ref };
     const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
         if( dx < -200) 
             return true;
@@ -30,6 +32,10 @@ const RenderDish = (props) => {
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: (e, gesture) => {
             return true;
+        },
+        onPanResponderGrant: () => {
+            view.rubberBand(1000)
+                .then(endState => console.log(endState.finished ? 'finished' : 'cancelled'))
         },
         onPanResponderEnd: (e, gestureState) => {
             if(recognizeDrag(gestureState)) 
@@ -46,7 +52,13 @@ const RenderDish = (props) => {
         }
     })
     if(dish)
-        return <Animatable.View animation="fadeInDown" duration={2000} delay={1000} {...panResponder.panHandlers} >
+        return <Animatable.View 
+                    animation="fadeInDown" 
+                    duration={2000} 
+                    delay={1000} 
+                    ref={handleViewRef}
+                    {...panResponder.panHandlers}
+                >
             <Card
                 featuredTitle={dish.name}
                 image={{ uri: baseUrl+dish.image }}
